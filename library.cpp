e5c3f8d1b7cbf210eff9631dab21688c70d83dff
ahmedhassanein <ahmedhassanein77777@gmail.com>
2023-04-06 01:57:18 +0200

    add read and save function

--- library.cpp
+++ library.cpp
@@ -17,13 +17,17 @@ struct student
 {
 	int ID;
 	string Name, borrowed_book[100];
+	string password;
 };
 
 const int MAX_SIZE = 1000;
+int n_borrowed_books;
+
 student students[MAX_SIZE];
 book books[MAX_SIZE];
 
-void save_data(int n);
+void save_data_books(int n);
+void save_data_students(int n);
 void read_data();
 
 void main() {
@@ -58,24 +62,89 @@ void main() {
 		cin.ignore();
 	}
 
-	save_data(nbooks);
+	save_data_books(nbooks);
 
+
+	int nStudents;
+
+	cout << "Enter number of students: ";
+	cin >> nStudents;
+	cin.ignore();
+
+	for (int i = 0; i < nStudents; i++) {
+		cout << "Enter student name: ";
+		getline(cin, students[i].Name);
+
+		cout << "Enter student password: ";
+		getline(cin, students[i].password);
+
+		cout << "Enter student ID: ";
+		cin >> students[i].ID;
+		cin.ignore();
+
+
+		cout << "Enter number of borrowed books: ";
+		cin >> n_borrowed_books;
+		cin.ignore();
+
+		cout << "Enter borrowed books names: ";
+		for (int j = 0; j < n_borrowed_books; j++) {
+			getline(cin, students[i].borrowed_book[j]);
+		}
+	}
+
+	save_data_students(nStudents);
+	
+	read_data();
 }
 
-void save_data(int n) {
+void save_data_books(int n) {
 	fstream file;
 	file.open("repo.txt", ios::out);
 
 	if (file.is_open()) {
 		for (int i = 0; i < n; i++) {
-			file << books[i].Name << endl;
-			file << books[i].Author_name << endl;
-			file << books[i].Category << endl;
-			file << books[i].Edition << endl;
-			file << books[i].Code << endl;
-			file << books[i].is_available << endl;
+			file << books[i].Name << '\n';
+			file << books[i].Author_name << '\n';
+			file << books[i].Category << '\n';
+			file << books[i].Edition << '\n';
+			file << books[i].Code << '\n';
+			file << books[i].is_available << '\n';
 		}
 		file.close();
 	}
 
+}
+
+
+void save_data_students(int n) {
+	fstream file;
+	file.open("repo.txt", ios::app);
+
+	if (file.is_open()) {
+		for (int i = 0; i < n; i++) {
+			file << students[i].Name << '\n';
+			file << students[i].ID << '\n';
+			file << students[i].password << '\n';
+			for (int j = 0; j < n_borrowed_books; j++) {
+				file << students[i].borrowed_book[j] << '\n';
+			}
+		}
+	}
+
+}
+
+
+void read_data() {
+	string word;
+
+	ifstream in_file;
+	
+	in_file.open("repo.txt", ios::in);
+	if (in_file.is_open()) {
+		while (getline(in_file, word)) {
+			cout << word << '\n';
+		}
+		in_file.close();
+	}
 }
